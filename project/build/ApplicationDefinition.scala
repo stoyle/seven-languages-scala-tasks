0109d5302592c0a94892516b16ef60c280185a8e
Alf Kristian Stoyle <alf.kristian@gmail.com>
2011-06-05 15:47:25 +0200

    Support for running plane JUnit.

--- project/build/ApplicationDefinition.scala
+++ project/build/ApplicationDefinition.scala
@@ -2,8 +2,12 @@ import sbt._
 
 class ApplicationProject(info: ProjectInfo) extends DefaultProject(info) {
 
+  override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
+  
   val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6-SNAPSHOT"% "test"
 	val junit = "junit" % "junit" % "4.8.2" % "test"
+	
+	val junitInterface = "com.novocode" %% "junit-interface" % "0.4.0" from "http://cloud.github.com/downloads/bryanjswift/junit-interface/junit-interface-0.4.0.jar"
 
 	val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
