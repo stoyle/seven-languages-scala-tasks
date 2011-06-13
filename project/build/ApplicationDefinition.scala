6be448f006fdf0722ceba60b706af073bee0919d
Fredrik Vraalsen <fredrik@vraalsen.no>
2011-06-14 00:52:24 +0200

    Fiks ScalaTest-versjon

--- project/build/ApplicationDefinition.scala
+++ project/build/ApplicationDefinition.scala
@@ -4,14 +4,14 @@ class ApplicationProject(info: ProjectInfo) extends DefaultProject(info) {
 
   override def testFrameworks = super.testFrameworks ++ List(new TestFramework("com.novocode.junit.JUnitFrameworkNoMarker"))
   
-  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6-SNAPSHOT" % "test"
-	val junit = "junit" % "junit" % "4.8.2" % "test"
+  val scalatest = "org.scalatest" % "scalatest_2.9.0" % "1.6.1" % "test"
+  val junit = "junit" % "junit" % "4.8.2" % "test"
 	
-	val junitInterface = "com.novocode" %% "junit-interface" % "0.4.0" from "http://cloud.github.com/downloads/bryanjswift/junit-interface/junit-interface-0.4.0.jar"
+  val junitInterface = "com.novocode" %% "junit-interface" % "0.4.0" from "http://cloud.github.com/downloads/bryanjswift/junit-interface/junit-interface-0.4.0.jar"
 
-	val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
 
-	override val mainCompilePath = "target" / "classes"
-	override val testCompilePath = "target" / "test-classes"
+  override val mainCompilePath = "target" / "classes"
+  override val testCompilePath = "target" / "test-classes"
 
 }
